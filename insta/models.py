030d9498caeab959530710f8f71ee7001553aa46
Ronyonka <rononyonka@gmail.com>
2019-03-11 02:19:40 +0300

    added profiles model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 from django.contriib.auth.models import User
-from tinymce
+import datetime as dt
+from tinymce.models import HTMLField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+
+class Profile(models.Model):
+    profile_picture = models.ImageField(blamk=True, upload_to='images/')
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
