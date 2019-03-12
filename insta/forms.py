e68b1a82271f05f529711cd0cf207e29b3db1790
Ronyonka <rononyonka@gmail.com>
2019-03-12 18:39:30 +0300

    added some changes

--- insta/forms.py
+++ insta/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile,Comments
 
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('image_path', 'caption')
+
+class  CommentsForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('text',)
 
 class ProfileForm(forms.ModelForm):
     class Meta:
