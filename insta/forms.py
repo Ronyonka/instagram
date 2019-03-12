2f36a7e0000f42f30512ddc43becd162a069c702
Ronyonka <rononyonka@gmail.com>
2019-03-12 13:18:22 +0300

    added edit profile form

--- insta/forms.py
+++ insta/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Image
+from .models import Image,Profile
 
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('image_path', 'caption')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
