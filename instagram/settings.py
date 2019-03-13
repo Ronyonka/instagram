dec44e3ce7a5f9a3cbe87f79de38a8f5e7e85ab4
Ronyonka <rononyonka@gmail.com>
2019-03-13 09:47:22 +0300

    deployed to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -108,12 +108,6 @@ AUTH_PROFILE_MODULE = 'accounts.Profile'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
